b10d195ae7614a3675ccdac23575e695351c0b23
bleymambwe <bleymambwe@gmail.com>
2021-06-26 21:03:58 +0200

    Linked list implimentation

---
+++ LinkedLists/Linked_List.py
@@ -0,0 +1,52 @@
+
+class Linkedlist:
+    def __init__(self):
+        self.head= None
+        self.next = None
+        self.len  = 0
+
+    def addData(self,data):
+        self.data= data
+
+    def append(self,data):
+        new_node = Linkedlist()
+        new_node.addData(data)
+
+        if self.head == None:
+            self.head = new_node
+            self.tail = self.head
+            self.len+= 1
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            self.len+= 1
+
+    def prepend(self,data):
+        new_node = Linkedlist()
+        new_node.addData(data)
+
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+            self.len+= 1
+        else:
+            new_node.next = self.head
+            self.head = new_node
+            self.len+= 1
+
+    def traverse(self):
+        cur = self.head
+        while cur:
+            print(cur.data)
+            cur = cur.next
+    def delete(self,pos):
+
+
+a = Linkedlist()
+#a.append(0)
+#a.append(2)
+#a.traverse()
+
+a.prepend(0)
+a.prepend(1)
+a.traverse()
